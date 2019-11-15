2ee8f64f695955c0153bdbea6784fc5b6d6faca3
kbiswa <kamal.biswa@wsu.edu>
2019-11-14 21:10:53 -0800

    basic version for part2 done

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -59,6 +59,7 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         ImageView ivProfileImage;
         TextView tvBody;
         TextView tvScreenName;
+        //TextView tvTimeStamps;
 
         public  ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -66,12 +67,14 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
             ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
             tvBody = itemView.findViewById(R.id.tvBody);
             tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            //tvTimeStamps = itemView.findViewById(R.id.tvTimeStamps);
         }
 
         public void bind(Tweet tweet) {
             tvBody.setText(tweet.body);
             tvScreenName.setText(tweet.user.screenName);
             Glide.with(context).load(tweet.user.profileImageUrl).into(ivProfileImage);
+            //tvTimeStamps.setText(tweet.timestamps);
         }
     }
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -3,21 +3,26 @@ package com.codepath.apps.restclienttemplate.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Tweet {
 
     public String body;
     public String createdAt;
     public User user;
+    //public String timestamps;
+    public Tweet(){} //empty constructor
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet  = new Tweet();
         tweet.body = jsonObject.getString("text");
         tweet.createdAt = jsonObject.getString(("created_at"));
         tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        //tweet.timestamps = jsonObject.getString("timestamps");
         return tweet;
     }
 
